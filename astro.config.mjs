ad46fc15c134b686fd02c631a5402991cb3d527c
Pablo Guallichico <pablo.guallichico@outlook.com>
2025-07-29 00:49:14 -0500

    refactor(build): update vite alias definition in astro config
    Refactored the Vite alias configuration in 'astro.config.mjs' to use the 'new URL()' constructor.
    
    This approach is a more modern and potentially more robust method for resolving paths in different environments compared to 'path.resolve', aiming to fix the 'ENOENT' error during the Cloudflare build.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,11 @@
 import { defineConfig } from 'astro/config';
-import { fileURLToPath } from 'url';
-import path from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, './src'),
+        '@': new URL('./src/', import.meta.url).pathname,
       },
     },
   },
